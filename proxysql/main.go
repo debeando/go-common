a25bcf7dad05b52f4e09a3cfb123b2e8e9aabcbe
Nicola Strappazzon C <nstrappazzonc@gmail.com>
2023-08-15 23:21:46 +0200

    add proxysql module

---
+++ proxysql/main.go
@@ -0,0 +1,130 @@
+package proxysql
+
+import (
+	"fmt"
+
+	"github.com/debeando/go-common/cast"
+	"github.com/debeando/go-common/mysql"
+)
+
+const (
+	ServerStatus string = ""
+	ONLINE              = "ONLINE"
+	OFFLINE_SOFT        = "OFFLINE_SOFT"
+	OFFLINE_HARD        = "OFFLINE_HARD"
+)
+
+type ProxySQL struct {
+	mysql.MySQL
+	Connection *mysql.Connection
+	Users      []User
+	Servers    []Server
+}
+
+type User struct {
+	DefaultHostgroup uint8  `json:"default_hostgroup"`
+	Active           bool   `json:"active"`
+	UseSSL           bool   `json:"use_ssl"`
+	Username         string `json:"username"`
+	Password         string `json:"password"`
+}
+
+type Server struct {
+	HostgroupID       uint8  `json:"hostgroup_id"`
+	Hostname          string `json:"hostname"`
+	MaxConnections    uint16 `json:"max_connections"`
+	MaxReplicationLag uint16 `json:"max_replication_lag"`
+	Status            string `json:"status"`
+	Weight            uint16 `json:"weight"`
+}
+
+func (p *ProxySQL) AddServer(s Server) int {
+	p.Servers = append(p.Servers, s)
+
+	return 0
+}
+
+func (p *ProxySQL) GetStatusServer(index int) string {
+	sql := fmt.Sprintf(
+		"SELECT status FROM mysql_servers WHERE hostgroup_id = %d AND hostname = '%s' LIMIT 1;",
+		p.Servers[index].HostgroupID,
+		p.Servers[index].Hostname,
+	)
+	result, _ := p.Connection.Query(sql)
+
+	if len(result) == 1 {
+		return result[0]["status"]
+	}
+	return ServerStatus
+}
+
+func (p *ProxySQL) SetStatusServer(index int, ss string) {
+	sql := fmt.Sprintf(
+		"UPDATE mysql_servers SET status = '%s' WHERE hostgroup_id = %d AND hostname = '%s';",
+		ss,
+		p.Servers[index].HostgroupID,
+		p.Servers[index].Hostname,
+	)
+	p.Connection.Query(sql)
+}
+
+func (p *ProxySQL) ExistServer(index int) bool {
+	sql := fmt.Sprintf(
+		"SELECT count() AS cnt FROM mysql_servers WHERE hostgroup_id = %d AND hostname = '%s';",
+		p.Servers[index].HostgroupID,
+		p.Servers[index].Hostname,
+	)
+	result, _ := p.Connection.Query(sql)
+
+	return ((len(result) == 1) && (cast.StringToInt(result[0]["cnt"]) == 1))
+}
+
+func (p *ProxySQL) DeleteServer(index int) {
+	sql := fmt.Sprintf(
+		"DELETE FROM mysql_servers WHERE hostgroup_id = %d AND hostname = '%s';",
+		p.Servers[index].HostgroupID,
+		p.Servers[index].Hostname,
+	)
+	p.Connection.Query(sql)
+}
+
+func (p *ProxySQL) InsertServer(index int) {
+	sql := fmt.Sprintf(
+		"INSERT INTO mysql_servers (hostgroup_id, hostname, status, max_connections, weight, max_replication_lag) VALUES (%d, '%s', '%s', %d, %d, %d);",
+		p.Servers[index].HostgroupID,
+		p.Servers[index].Hostname,
+		ONLINE,
+		p.Servers[index].MaxConnections,
+		p.Servers[index].Weight,
+		p.Servers[index].MaxReplicationLag,
+	)
+
+	p.Connection.Query(sql)
+}
+
+func (p *ProxySQL) EnableServer(index int) {
+	if p.ExistServer(index) && p.GetStatusServer(index) == OFFLINE_HARD {
+		p.DeleteServer(index)
+		p.InsertServer(index)
+	}
+
+	if p.ExistServer(index) && p.GetStatusServer(index) == OFFLINE_SOFT {
+		p.SetStatusServer(index, ONLINE)
+	}
+
+	if !p.ExistServer(index) {
+
+		p.InsertServer(index)
+	}
+
+	p.LoadServers()
+	p.SaveServers()
+}
+
+func (p *ProxySQL) LoadServers() {
+	p.Connection.Query("LOAD MYSQL SERVERS TO RUNTIME;")
+}
+
+func (p *ProxySQL) SaveServers() {
+	p.Connection.Query("SAVE MYSQL SERVERS TO DISK;")
+}

554745b7ed5bffee9b1893a23b392863c14c37be
Nicola Strappazzon C <nstrappazzonc@gmail.com>
2023-10-11 21:50:47 +0200

    fix - rename method ParseValue to ParseNumberValue.

--- mysql/main_test.go
+++ mysql/main_test.go
@@ -7,43 +7,43 @@ import (
 )
 
 func TestParseValue(t *testing.T) {
-	if value, ok := mysql.ParseValue("yes"); !ok || value != 1 {
+	if value, ok := mysql.ParseNumberValue("yes"); !ok || value != 1 {
 		t.Error("Expected: Found Parse and value = 1.")
 	}
 
-	if value, ok := mysql.ParseValue("Yes"); !ok || value != 1 {
+	if value, ok := mysql.ParseNumberValue("Yes"); !ok || value != 1 {
 		t.Error("Expected: Found Parse and value = 1.")
 	}
 
-	if value, ok := mysql.ParseValue("YES"); !ok || value != 1 {
+	if value, ok := mysql.ParseNumberValue("YES"); !ok || value != 1 {
 		t.Error("Expected: Found Parse and value = 1.")
 	}
 
-	if value, ok := mysql.ParseValue("no"); !ok || value != 0 {
+	if value, ok := mysql.ParseNumberValue("no"); !ok || value != 0 {
 		t.Error("Expected: Found Parse and value = 0.")
 	}
 
-	if value, ok := mysql.ParseValue("No"); !ok || value != 0 {
+	if value, ok := mysql.ParseNumberValue("No"); !ok || value != 0 {
 		t.Error("Expected: Found Parse and value = 0.")
 	}
 
-	if value, ok := mysql.ParseValue("NO"); !ok || value != 0 {
+	if value, ok := mysql.ParseNumberValue("NO"); !ok || value != 0 {
 		t.Error("Expected: Found Parse and value = 0.")
 	}
 
-	if value, ok := mysql.ParseValue("ON"); !ok || value != 1 {
+	if value, ok := mysql.ParseNumberValue("ON"); !ok || value != 1 {
 		t.Error("Expected: Found Parse and value = 1.")
 	}
 
-	if value, ok := mysql.ParseValue("OFF"); !ok || value != 0 {
+	if value, ok := mysql.ParseNumberValue("OFF"); !ok || value != 0 {
 		t.Error("Expected: Found Parse and value = 0.")
 	}
 
-	if value, ok := mysql.ParseValue("true"); ok && value == 0 {
+	if value, ok := mysql.ParseNumberValue("true"); ok && value == 0 {
 		t.Error("Expected: Imposible Parse.")
 	}
 
-	if value, ok := mysql.ParseValue("1234567890"); !ok || value != 1234567890 {
+	if value, ok := mysql.ParseNumberValue("1234567890"); !ok || value != 1234567890 {
 		t.Error("Expected: Found Parse and value = 1234567890.")
 	}
 }

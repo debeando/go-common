b1cb0f1c9b48f83e86144a7ea9bbfe536079cbaf
Nicola Strappazzon C <nstrappazzonc@gmail.com>
2023-10-17 09:05:09 +0200

    refact - mongodb

--- mongodb/main.go
+++ mongodb/main.go
@@ -34,23 +34,59 @@ func (c *Connection) Connect() (err error) {
 	clientOptions := options.Client().ApplyURI(c.DSN)
 	c.Client, err = mongo.Connect(c.Context, clientOptions)
 	if err != nil {
-		log.ErrorWithFields("MongoDB", log.Fields{"message": err})
+		log.ErrorWithFields("MongoDB:Connect", log.Fields{"message": err})
 		return err
 	}
 
 	err = c.Client.Ping(c.Context, nil)
 	if err != nil {
-		log.ErrorWithFields("MongoDB", log.Fields{"message": err})
+		log.ErrorWithFields("MongoDB:Connect:Ping", log.Fields{"message": err})
 		return err
 	}
 
 	return nil
 }
 
-func (c *Connection) GetServerStatus() (ss map[string]interface{}) {
-	db := c.Client.Database("admin")
-	cmd := bson.D{primitive.E{Key: "serverStatus", Value: "1"}}
-	db.RunCommand(context.TODO(), cmd).Decode(&ss)
+func (c *Connection) Close() {
+	if err := c.Client.Disconnect(c.Context); err != nil {
+		log.ErrorWithFields("MongoDB:Close", log.Fields{"message": err})
+	}
+}
 
-	return ss
+func (c *Connection) RunCommand(database string, cmd interface{}, result interface{}) error {
+	db := c.Client.Database(database)
+	return db.RunCommand(context.TODO(), cmd).Decode(result)
+}
+
+func (c *Connection) ServerStatus() *ServerStatus {
+	serverstatus := &ServerStatus{}
+	cmd := bson.D{primitive.E{Key: "serverStatus", Value: "1"}}
+	c.RunCommand("admin", cmd, serverstatus)
+
+	return serverstatus
+}
+
+func (c *Connection) Databases() *Databases {
+	databases := &Databases{}
+	cmd := bson.D{primitive.E{Key: "listDatabases", Value: "1"}}
+	c.RunCommand("admin", cmd, databases)
+
+	return databases
+}
+
+func (c *Connection) Collections(database string) []string {
+	colls, err := c.Client.Database(database).ListCollectionNames(context.TODO(), bson.M{})
+	if err != nil {
+		log.ErrorWithFields("MongoDB:Collections", log.Fields{"message": err})
+	}
+
+	return colls
+}
+
+func (c *Connection) CollectionStats(dbName, colName string) *CollectionStats {
+	collectionStats := &CollectionStats{}
+	cmd := bson.D{primitive.E{Key: "collStats", Value: colName}}
+	c.RunCommand(dbName, cmd, collectionStats)
+
+	return collectionStats
 }

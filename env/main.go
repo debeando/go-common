65cda7a1d7a807d2a2534c9efc872850f08ebb35
Nicola Strappazzon C <nstrappazzonc@gmail.com>
2024-01-19 22:22:02 +0100

    feat - add check exist env var.

--- env/main.go
+++ env/main.go
@@ -4,6 +4,14 @@ import (
 	"os"
 )
 
+// Verify environment variable.
+func Exist(key string) bool {
+	if _, ok := os.LookupEnv(key); ok {
+		return true
+	}
+	return false
+}
+
 // Retrieve environment variable.
 func Get(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
--- env/main_test.go
+++ env/main_test.go
@@ -13,3 +13,10 @@ func TestGet(t *testing.T) {
 	assert.Equal(t, env.Get("ENV_VAR", "value2"), "value1")
 	assert.Equal(t, env.Get("ENV_TMP", "value1"), "value1")
 }
+
+func TestExist(t *testing.T) {
+	t.Setenv("ENV_VAR", "value1")
+
+	assert.True(t, env.Exist("ENV_VAR"))
+	assert.False(t, env.Exist("ENV_TMP"))
+}

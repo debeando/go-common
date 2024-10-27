77ce88f7a89ecc6f6b7fa53e98ec99b9d1bbd7f9
Nicola Strappazzon <nicola@strappazzon.me>
2024-10-27 10:24:02 +0100

    feat - in env submodule is add GetInt, GetUInt8, GetUInt16 and GetBool.

--- env/main.go
+++ env/main.go
@@ -2,9 +2,10 @@ package env
 
 import (
 	"os"
+	"strconv"
 )
 
-// Verify environment variable.
+// Verify exist environment variable.
 func Exist(key string) bool {
 	if _, ok := os.LookupEnv(key); ok {
 		return true
@@ -12,10 +13,49 @@ func Exist(key string) bool {
 	return false
 }
 
-// Retrieve environment variable.
+// Retrieve environment variable in String type.
 func Get(key, fallback string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
 	}
+	return fallback
+}
+
+// Retrieve environment variable in Int type.
+func GetInt(key string, fallback int) int {
+	if envvalue, ok := os.LookupEnv(key); ok {
+		value, _ := strconv.Atoi(envvalue)
+		return value
+	}
+
+	return fallback
+}
+
+// Retrieve environment variable in Int type.
+func GetUInt8(key string, fallback uint8) uint8 {
+	if envvalue, ok := os.LookupEnv(key); ok {
+		value, _ := strconv.Atoi(envvalue)
+		return uint8(value)
+	}
+
+	return fallback
+}
+
+func GetUInt16(key string, fallback uint16) uint16 {
+	if envvalue, ok := os.LookupEnv(key); ok {
+		value, _ := strconv.Atoi(envvalue)
+		return uint16(value)
+	}
+
+	return fallback
+}
+
+// Retrieve environment variable in Bool type.
+func GetBool(key string, fallback bool) bool {
+	if envvalue, ok := os.LookupEnv(key); ok {
+		value, _ := strconv.ParseBool(envvalue)
+		return value
+	}
+
 	return fallback
 }
